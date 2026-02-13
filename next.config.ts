9d7c35a481c2bab72419fce34013fff1d4ce7e07
Amartya <amartya@cp.dev>
2026-02-13 12:17:39 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/:path*"
-            : "/api/",
-      },
-    ];
+    return process.env.NODE_ENV === "development"
+      ? [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8000/:path*",
+        },
+      ]
+      : [];
   },
 };
 
