bc020790083ffe1be640d7c3d0fd2ba67be7898e
Amartya <amartya@cp.dev>
2026-02-13 11:50:37 +0530

    Fix 404: Set FastAPI root_path to /api

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from tracer_python import trace_python_code
 # from tracer_cpp import trace_cpp_code # implementation pending
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.add_middleware(
     CORSMiddleware,
